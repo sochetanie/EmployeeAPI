1c095fdfab1ace6c3785f7e9bd431edde14a504f
github username <github email address>
2019-05-16 21:52:55 +0300

    added dlete and update from CRUD emp

--- src/main/java/com/cognizant/employeedb/Services/EmployeeService.java
+++ src/main/java/com/cognizant/employeedb/Services/EmployeeService.java
@@ -18,12 +18,55 @@ public class EmployeeService {
     }
 
     public Iterable<Employee> getAllEmployees() {
-        return this.repository.findAll();
+      return this.repository.findAll();
     }
 
     public Employee saveEmployee(Employee employee) {
-        return this.repository.save(employee);
+      return this.repository.save(employee);
     }
 
-    public Optional<Employee> getEmployeeById(long id) { return this.repository.findById(id); }
+    public Optional<Employee> getEmployeeById(long id) {
+      return this.repository.findById(id);
+    }
+
+  public boolean delete(Long id){
+    if (repository.findById(id).isPresent()) {
+      repository.deleteById(id);
+      return true;
+    }
+    return false;
+  }
+
+  public Optional<Employee> update(Long id, Employee employee){
+    Optional<Employee> empOptional = repository.findById(id);
+    if (empOptional.isPresent()) {
+      Employee emp = empOptional.get();
+      if (employee.getName() != null) {
+        emp.setName(employee.getName());
+      }
+      if (employee.getAddress() != null) {
+        emp.setAddress(employee.address());
+      }
+      if (employee.getCity() != null) {
+        emp.setCity(employee.city());
+      }
+      if (employee.getState() != null) {
+        emp.setState(employee.state());
+      }
+      if (employee.getZip() != null) {
+        emp.setZip(employee.zip());
+      }
+      if (employee.getPhoneNumber() != null) {
+        emp.setPhoneNumber(employee.phoneNumber());
+      }
+      if (employee.getJobTitle() != null) {
+        emp.setJobTitle(employee.jobTitle());
+      }
+
+      repository.save(emp);
+      return Optional.of(emp);
+    }
+    return Optional.empty();
+  }
+
 }
