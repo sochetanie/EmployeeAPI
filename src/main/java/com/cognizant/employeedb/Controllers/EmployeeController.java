05180dbe98afc5ec0ac46d2e633bf401a91157da
github username <github email address>
2019-05-23 12:46:29 +0300

    fixed some imports

--- src/main/java/com/cognizant/employeedb/Controllers/EmployeeController.java
+++ src/main/java/com/cognizant/employeedb/Controllers/EmployeeController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/employee")
 @CrossOrigin(origins = "http://localhost:4200")
+@Api(value="Employee Management System", description="Operations pertaining to employee in Employee Management System")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
@@ -18,28 +20,39 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
-    @GetMapping("/all")
-    public Iterable<Employee> all(){
+  @ApiOperation(value = "View a list of available employees", response = List.class)
+  @ApiResponses(value = {
+      @ApiResponse(code = 200, message = "Successfully retrieved list"),
+      @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+      @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+      @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+  })
+  @GetMapping("/all")
+    public List<Employee> all(){
         return employeeService.getAllEmployees();
     }
 
-    @PostMapping("/")
-    public Employee save(@RequestBody Employee employee) {
-        return employeeService.saveEmployee(employee);
+  @ApiOperation(value = "Add an employee")
+  @PostMapping("/")
+  public Employee save(@ApiParam(value = "Employee object store in database table", required = true) @Valid @RequestBody Employee employee) {
+    return employeeService.saveEmployee(employee);
     }
 
-    @GetMapping("/{id}")
-    public Optional<Employee> getById(@PathVariable("id") Long id) {
-        return employeeService.getEmployeeById(id);
-    }
+  @ApiOperation(value = "Get an employee by Id")
+  @GetMapping("/{id}")
+  public Optional<Employee> getById(@PathVariable("id") Long id) {
+      return employeeService.getEmployeeById(id);
+  }
 
-    @PutMapping("/{id}")
-    public Optional<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee newEmployee) {
-      return employeeService.update(id, newEmployee);
-    }
+  @ApiOperation(value = "Update an employee")
+  @PutMapping("/{id}")
+  public Optional<Employee> updateEmployee(@ApiParam(value = "Employee Id to update employee object", required = true) @PathVariable(value = "id") Long id, @ApiParam(value = "Update employee object", required = true) @Valid @RequestBody Employee employeeDetails) {
+    return employeeService.update(id, employeeDetails);
+  }
 
-    @DeleteMapping("/{id}")
-    public boolean deleteEmployee(@PathVariable Long id){
-      return employeeService.delete(id);
-    }
+  @ApiOperation(value = "Delete an employee")
+  @DeleteMapping("/{id}")
+  public boolean deleteEmployee(@ApiParam(value = "Employee Id from which employee object will delete from database table", required = true) @PathVariable(value = "id") Long id) {
+    return employeeService.delete(id);
+  }
 }
